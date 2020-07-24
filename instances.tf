9c3f3f14aad20731bbd1fc6764d594b0a33436ef
adriangarciapulido <68708339+adriangarciapulido@users.noreply.github.com>
2020-07-24 17:15:53 +0200

    Add files via upload

--- instances.tf
+++ instances.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "nat" {
-  ami                         = "ami-0ba1c3ef9156479a2"
+  ami                         = "ami-0eec727d3bec65ade"
   availability_zone           = "ap-southeast-1b"
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
